69ffde453055142a2d142c86dfe24141eb9d34a9
skwan <skwan@microsoft.com>
2014-04-01 10:40:15 -0700

    Add license headers, move all config items to config

--- README.md
+++ README.md
@@ -96,11 +96,12 @@ NOTE:  Steps 13 and 14 are not necessary for this sample, but are included in ca
 
 #### Configure the TodoListDaemon project
 
-1. Open `Program.cs'.
-2. Find the declaration of `tenant` and replace the value with the name of your Azure AD tenant.
-3. Find the declaration of `clientId` and replace the value with the Client ID from the Azure portal.
-4. Find the declaration of `appKey` and replace the value with the Key from the Azure portal.
-5. Find the declaration of `todoListResourceId` and `todoListBaseAddress` and ensure their values are set properly for your TodoListService project.
+1. Open `app.config'.
+2. Find the app key `ida:Tenant` and replace the value with your AAD tenant name.
+3. Find the app key `ida:ClientId` and replace the value with the Client ID for the TodoListClient from the Azure portal.
+4. Find the app key `ida:AppKey` and replace the value with the key for the TodoListClient from the Azure portal.
+5. Find the app key `todo:TodoListResourceId` and replace the value with the  App ID URI of the TodoListService, for example `https://<your_tenant_name>/TodoListService`
+6. Find the app key `todo:TodoListBaseAddress` and replace the value with the base address of the TodoListService project.
 
 ### Step 5:  Trust the IIS Express SSL certificate
 
@@ -136,8 +137,9 @@ First, in Visual Studio 2013 create an empty solution to host the  projects.  Th
 
 1. In the solution, create a new Windows --> Console Application called TodoListDaemon.
 2. Add the (stable) Active Directory Authentication Library (ADAL) NuGet, Microsoft.IdentityModel.Clients.ActiveDirectory, version 1.0.3 (or higher) to the project.
-3. Add  assembly references to `System.Net.Http` and `System.Web.Extensions`.
+3. Add  assembly references to `System.Net.Http`, `System.Web.Extensions`, and `System.Configuration`.
 4. Add a new class to the project called `TodoItem.cs`.  Copy the code from the sample project file of same name into this class, completely replacing the code in the file in the new project.
 5. Copy the code from `Program.cs` in the sample project into the file of same name in the new project, completely replacing the code in the file in the new project.
+6. In `app.config` create keys for `ida:AADInstance`, `ida:Tenant`, `ida:ClientId`, `ida:AppKey`, `todo:TodoListResourceId`, and `todo:TodoListBaseAddress` and set them accordingly.  For the public Azure cloud, the value of `ida:AADInstance` is `https://login.windows.net/{0}`.
 
 Finally, in the properties of the solution itself, set both projects as startup projects.
--- TodoListDaemon/Program.cs
+++ TodoListDaemon/Program.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿//----------------------------------------------------------------------------------------------
+//    Copyright 2014 Microsoft Corporation
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//----------------------------------------------------------------------------------------------
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// The following using statements were added for this sample.
 using System.Globalization;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System.Net.Http;
 using System.Threading;
 using System.Net.Http.Headers;
 using System.Web.Script.Serialization;
+using System.Configuration;
 
 namespace TodoListDaemon
 {
@@ -22,10 +40,10 @@ namespace TodoListDaemon
         // The AAD Instance is the instance of Azure, for example public Azure or Azure China.
         // The Authority is the sign-in URL of the tenant.
         //
-        const string aadInstance = "https://login.windows.net/{0}";
-        const string tenant = "skwantoso.com";
-        const string clientId = "a2f3e366-3dd9-4597-8445-ee8cab4eb65a";
-        const string appKey = "NgT0Xhr71BoWyVgnNzmLAeo2TfRREh2nlQDx4gztOLk=";
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string appKey = ConfigurationManager.AppSettings["ida:AppKey"];
 
         static string authority = String.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
 
@@ -33,8 +51,8 @@ namespace TodoListDaemon
         // To authenticate to the To Do list service, the client needs to know the service's App ID URI.
         // To contact the To Do list service we need it's URL as well.
         //
-        const string todoListResourceId = "https://skwantoso.com/TodoListService";
-        const string todoListBaseAddress = "https://localhost:44321";
+        private static string todoListResourceId = ConfigurationManager.AppSettings["todo:TodoListResourceId"];
+        private static string todoListBaseAddress = ConfigurationManager.AppSettings["todo:TodoListBaseAddress"];
 
         private static HttpClient httpClient = new HttpClient();
         private static AuthenticationContext authContext = null;
@@ -187,7 +205,7 @@ namespace TodoListDaemon
                 int count = 0;
                 foreach (TodoItem item in toDoArray)
                 {
-                    Console.WriteLine("Owner: {0}\nItem:  {1}", item.Owner, item.Title);
+                    Console.WriteLine(item.Title);
                     count++;
                 }
 
